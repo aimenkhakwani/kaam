32b14c55faf1a888eac027f3ae385da2155483e6
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-12-21 12:15:12 -0800

    Add a custom module to redirect authenticated users to home page on submit of new product

--- sites/all/themes/soar/templates/views-view-unformatted.tpl.php
+++ sites/all/themes/soar/templates/views-view-unformatted.tpl.php
@@ -10,8 +10,8 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-    <div class="col-md-3 kaamEachProduct">
-        <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <div class="col-md-3">
+        <div class="kaamEachItem">
             <?php print $row; ?>
         </div>
     </div>
