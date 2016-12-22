d77003da49b6ec33ce2d1e759c1ad52bf78ea2d7
aimenkhakwani <aimen.khakwani@hotmail.com>
2016-12-21 16:03:09 -0800

    Remove cart link unless users are logged in

--- sites/all/themes/soar/templates/page.tpl.php
+++ sites/all/themes/soar/templates/page.tpl.php
@@ -6,13 +6,11 @@
                 <?php print render($page['user_menu']); ?>
             <?php elseif ($is_front): ?>
                 <ul>
-                    <li><a href="<?php $front_page; ?>cart">Cart</a></li>
                     <li><a href="<?php $front_page; ?>user/login">Log In</a></li>
                     <li><a href="<?php $front_page; ?>user/register">Register</a></li>
                 </ul>
             <?php else: ?>
                 <ul>
-                    <li><a href="<?php $front_page; ?>cart">Cart</a></li>
                     <li><a href="<?php $front_page; ?>login">Log In</a></li>
                     <li><a href="<?php $front_page; ?>register">Register</a></li>
                 </ul>
